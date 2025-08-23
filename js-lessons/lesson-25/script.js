9411b30165d792a31891d4ebe62aba7b0eba32aa
Daria Steblovska <darias1896.96@gmail.com>
2025-08-23 03:36:26 +0100

    home tasks and training fetch

---
+++ js-lessons/lesson-25/script.js
@@ -0,0 +1,61 @@
+// Задача. Використовуючи один з АРІ
+// https://github.com/public-apis/public-apis#animals
+// та функцію fetch організувати завантаження та відображення даних
+
+const container = document.querySelector("#container");
+const image = document.querySelector("#image");
+
+// =============== dogs fetch ==================
+// fetch("https://dog.ceo/api/breeds/image/random")
+//   .then((response) => {
+//     console.log("----response", response);
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log("----data", data);
+//     image.src = data.message;
+//     return data;
+//   });
+
+// =============== books fetch ==================
+// let promptAns = prompt("Book title?");
+
+// fetch(`https://www.googleapis.com/books/v1/volumes?q=${promptAns}+terms`)
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data);
+//     data.items.forEach((element) => {
+//       //   console.log(element.volumeInfo.title);
+//       //   console.log(element.volumeInfo.authors);
+//       //   console.log(element.volumeInfo.imageLinks);
+//       image.src = element.volumeInfo.imageLinks.thumbnail;
+//       container.innerText = element.volumeInfo.title;
+//     });
+//   });
+
+const bookInput = document.querySelector("#book-input");
+const findBtn = document.querySelector("#find");
+
+// +inauthor:${bookInput}
+
+findBtn.addEventListener("click", () => {
+  fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=intitle:${bookInput.value.trim()}`
+  )
+    .then((response) => {
+      console.log("---response", response);
+      return response.json();
+    })
+    .then((data) => {
+      console.log("---data", data);
+      data.items.forEach((book) => {
+        const bookImage = document.createElement("img");
+        const bookTitle = document.createElement("h2");
+        bookTitle.innerText = book.volumeInfo.title;
+        bookImage.src = book.volumeInfo.imageLinks.thumbnail;
+        document.body.append(bookTitle);
+        document.body.append(bookImage);
+      });
+    })
+    .catch((err) => console.log('----err', err));
+});
